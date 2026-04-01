69a9637676b1c84d78ab99c36efc144cb7919072
kacperg <kacpergcloud@gmail.com>
2026-04-01 19:21:09 +0200

    first commit

---
+++ src/main/java/pl/gfm/system_zarzadzania_linia_lotnicza/SystemZarzadzaniaLiniaLotniczaApplication.java
@@ -0,0 +1,13 @@
+package pl.gfm.system_zarzadzania_linia_lotnicza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemZarzadzaniaLiniaLotniczaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemZarzadzaniaLiniaLotniczaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gfm/system_zarzadzania_linia_lotnicza/SystemZarzadzaniaLiniaLotniczaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gfm.system_zarzadzania_linia_lotnicza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemZarzadzaniaLiniaLotniczaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
