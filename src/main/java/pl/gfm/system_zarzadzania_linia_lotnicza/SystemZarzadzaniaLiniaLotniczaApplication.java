c6d4fc8a99529206fca68cb3fd9274b0dcb5699d
kacperg <kacpergcloud@gmail.com>
2026-04-01 19:23:33 +0200

    feat: test comit

--- src/main/java/pl/gfm/system_zarzadzania_linia_lotnicza/SystemZarzadzaniaLiniaLotniczaApplication.java
+++ src/main/java/pl/gfm/system_zarzadzania_linia_lotnicza/SystemZarzadzaniaLiniaLotniczaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SystemZarzadzaniaLiniaLotniczaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SystemZarzadzaniaLiniaLotniczaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SystemZarzadzaniaLiniaLotniczaApplication.class, args);
+    }
 
 }
